2005f7076b0e22c92df9b0099c187fe7ea18d4bf
Ginakira <ginakira@foxmail.com>
2020-06-04 18:21:47 +0800

    UDP

--- server/server.c
+++ server/server.c
@@ -83,10 +83,8 @@ int main(int argc, char **argv) {
         int nfds = epoll_wait(epoll_fd, events, MAX * 2, -1);
 
         for (int i = 0; i < nfds; ++i) {
-            char info[1024];
+            char info[1024] = {0};
             recvfrom(events[i].data.fd, (void *)&lg, sizeof(lg), 0,
-                     (struct sockaddr *)&client, &len);
-            recvfrom(listener, (void *)&lg, sizeof(lg), 0,
                      (struct sockaddr *)&client, &len);
             sprintf(info, "Login: %s:%d\n", inet_ntoa(client.sin_addr),
                     ntohs(client.sin_port));
