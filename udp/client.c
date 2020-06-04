414a31e19fb5b91d710838a7529da9b2406c2b9c
Ginakira <ginakira@foxmail.com>
2020-06-04 18:59:06 +0800

    UDP

--- udp/client.c
+++ udp/client.c
@@ -1,8 +1,44 @@
-/*************************************************************************
-	> File Name: client.c
-	> Author: 
-	> Mail: 
-	> Created Time: Thu 04 Jun 2020 06:21:24 PM CST
- ************************************************************************/
+/************************************************************
+        File Name : client.c
+        Author: Ginakira
+        Mail: ginakira@outlook.com
+        Github: https://github.com/Ginakira
+        Created Time: 2020/06/04 18:22:40
+************************************************************/
 
-#include "../common"
+#include "../common/color.h"
+#include "../common/common.h"
+#include "../common/head.h"
+#include "../common/udp_client.h"
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s ip port!\n", argv[0]);
+        exit(1);
+    }
+    char ip[20] = {0};
+    int port = atoi(argv[2]);
+    strncpy(ip, argv[1], 20);
+
+    char msg[512] = {0};
+    int sockfd;
+    struct sockaddr_in server;
+    socklen_t len = sizeof(server);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(ip);
+
+    if ((sockfd = socket_udp()) < 0) {
+        perror("socket_udp");
+        exit(1);
+    }
+
+    sendto(sockfd, "hi", sizeof("hi"), 0, (struct sockaddr *)&server, len);
+
+    sleep(1);
+
+    recvfrom(sockfd, msg, sizeof(msg), 0, (struct sockaddr *)&server, &len);
+
+    printf("D : recv from server %s\n", msg);
+    return 0;
+}
